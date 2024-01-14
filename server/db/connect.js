314de4b59eee3de0d5c75907803022716e333466
Bassem.A.Alameddine <balamddine@gmail.com>
2024-01-14 20:57:02 +0200

    test

--- server/db/connect.js
+++ server/db/connect.js
@@ -29,8 +29,9 @@ module.exports = {
 
             await client.connect();
             // Send a ping to confirm a successful connection
-            await client.db("chat").command({ ping: 1 });
+           let connected = await client.db("chat").command({ ping: 1 });
             console.log("Pinged your deployment. You successfully connected to MongoDB!");
+            return connected;
         }
         catch (ex) {
             console.log(ex)
