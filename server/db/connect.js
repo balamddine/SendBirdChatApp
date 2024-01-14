81fd812d68a359c43f90af09d29daa0fab41bf92
Bassem.A.Alameddine <balamddine@gmail.com>
2024-01-14 20:35:42 +0200

    test

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,22 +1,43 @@
 const mongoose = require("mongoose");
 const config = require("./config.json")
+const { MongoClient, ServerApiVersion } = require('mongodb');
 module.exports = {
     connect: async () => {
-        try {
-            // Replace the uri string with your connection string.
-            let client = await mongoose.connect(config.dbURI, {
-                useNewUrlParser: true,                
-                useUnifiedTopology: true,                
-            });
-            if(client){
-                return client;
+        const connStrng = "mongodb+srv://balamddine:Ayxq3Xz1bvhIZzqJ@cluster0.ot6f11x.mongodb.net/?retryWrites=true&w=majority" //"mongodb://127.0.0.1:27017/chat" 
+        const client = new MongoClient(connStrng, {
+            serverApi: {
+                version: ServerApiVersion.v1,
+                strict: true,
+                deprecationErrors: true,
             }
-            else{
-                return null;
-            }            
+        });
+        try {
+
+            // Replace the uri string with your connection string.
+            // let client = await mongoose.connect(connStrng, {
+            //     useNewUrlParser: true,                
+            //     useUnifiedTopology: true,                
+            // });
+            // if(client){
+            //     return client;
+            // }
+            // else{
+            //     return null;
+            // }            
+
+          
+
+            await client.connect();
+            // Send a ping to confirm a successful connection
+            await client.db("chat").command({ ping: 1 });
+            console.log("Pinged your deployment. You successfully connected to MongoDB!");
         }
         catch (ex) {
             console.log(ex)
+        }
+        finally {
+            // Ensures that the client will close when you finish/error
+            await client.close();
         }
     }
 }
